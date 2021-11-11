6ab979cbf1466fcae1f02237b241abfdbe7f7448
Angel Gerardo Jimenez Reyes <angel.g.j.reyes@outlook.com>
2021-11-11 14:19:10 -0600

    + Modelo CuentaVoluntario

---
+++ app/src/main/java/models/CTAVoluntario.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.util.Date;
+
+public class CTAVoluntario {
+    public String IDCTAMigrante;
+    public String Username;
+    public String PasswordHash;
+    public String Email;
+    public Date BirthDate;
+    public String Gender;
+    public String Type;
+}
