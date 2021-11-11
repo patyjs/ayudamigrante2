33f1449ddce0dcbd6550e32da9b5121f67748a37
Angel Gerardo Jimenez Reyes <angel.g.j.reyes@outlook.com>
2021-11-11 13:52:04 -0600

    + Modelo CuentaMigrante

---
+++ app/src/main/java/models/CTAMigrante.java
@@ -0,0 +1,12 @@
+package models;
+
+import java.util.Date;
+
+public class CTAMigrante {
+    public String IDCTAMigrante;
+    public String Username;
+    public String PasswordHash;
+    public String Email;
+    public Date BirthDate;
+    public String Gender;
+}
